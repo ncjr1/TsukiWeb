71c2c375463a8d6e7a18b323fcc1fc1e6edef1cb
Nilton Cosme <ncjr1@outlook.com>
2021-12-19 17:49:06 -0300

    [Index] Carrossel Funcionando

---
+++ TsukiWeb/wwwroot/js/Index.js
@@ -0,0 +1,24 @@
+﻿class Index {
+    constructor(){
+        this.Iniciar();
+    }
+
+    Iniciar(){
+        new Glider(document.querySelector('.glider'), {
+            slidesToShow: 1,
+            dots: '.dots',
+            draggable: true,
+            arrows: {
+              prev: '.glider-prev',
+              next: '.glider-next'
+            },
+            rewind: true
+          });
+
+    }
+    
+}
+
+$(document).ready(function () {
+    app = new Index();
+});
