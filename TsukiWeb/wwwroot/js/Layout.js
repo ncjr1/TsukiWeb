476ca7a4ec8ec97d88243b12a9df517add61065d
Nilton <Nilton@DESKTOP-S08EF52>
2021-12-19 14:36:36 -0300

    Adicionar arquivos de projeto.

---
+++ TsukiWeb/wwwroot/js/Layout.js
@@ -0,0 +1,46 @@
+﻿// Layout Master JS
+
+//Scroll Resize 
+window.onscroll = function() { ScrollResize() };
+
+var header = $("#myHeader");
+var logo = $("#logo");
+var searchBar = $("#searchbar");
+var headerBackground = $("#header");
+var background = $("#background");
+var login = $("#login");
+var navHeader = $("#navHeader");
+var sticky = header.offsetTop;
+
+function ScrollResize() {
+  if (window.pageYOffset > 50) {
+    header.addClass("sticky");
+    headerBackground.addClass("resizeHeader");
+
+    logo.addClass("resizeLogo1");
+    logo.removeClass("resizeLogo2");
+
+    searchBar.addClass("resizeBar1");
+    searchBar.removeClass("resizeBar2");
+    
+    login.addClass("resizeLogin1");
+    login.removeClass("resizeLogin2");
+
+    navHeader.addClass("navHeader");
+  } else {
+    header.removeClass("sticky");
+    headerBackground.removeClass("resizeHeader");
+
+    logo.removeClass("resizeLogo1");
+    logo.addClass("resizeLogo2");
+
+    searchBar.removeClass("resizeBar1");
+    searchBar.addClass("resizeBar2");
+
+    login.removeClass("resizeLogin1");
+    login.addClass("resizeLogin2");
+    
+
+    navHeader.removeClass("navHeader");
+  }
+}
