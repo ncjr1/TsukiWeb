901d5db00697ed1250e73660ae4883b1dc1f49ff
Nilton Cosme <ncjr1@outlook.com>
2021-12-24 22:20:12 -0300

    Consertado o Transition do header, Refeito o header do zero. Ta lindo caralho

--- TsukiWeb/wwwroot/js/Layout.js
+++ TsukiWeb/wwwroot/js/Layout.js
@@ -4,43 +4,22 @@
 window.onscroll = function() { ScrollResize() };
 
 var header = $("#myHeader");
-var logo = $("#logo");
-var searchBar = $("#searchbar");
-var headerBackground = $("#header");
-var background = $("#background");
-var login = $("#login");
-var navHeader = $("#navHeader");
 var sticky = header.offsetTop;
 
 function ScrollResize() {
-  if (window.pageYOffset > 50) {
-    $("#myHeader").addClass("sticky");
-    $("#header").addClass("resizeHeader");
-
-    $("#logo").addClass("resizeLogo1");
-    $("#logo").removeClass("resizeLogo2");
-
-    $("#searchbar").addClass("resizeBar1");
-    $("#searchbar").removeClass("resizeBar2");
-    
-    $("#login").addClass("resizeLogin1");
-    $("#login").removeClass("resizeLogin2");
-
-    $("#navHeader").addClass("navHeader");
+  if (window.pageYOffset > 10) {
+    $("#header").addClass("transition");
+    $("#header").addClass("sticky");
+    $("#logo").addClass("resizeLogo");
+    $("#searchbar").addClass("resizeSearchBar");
+    $("#login").addClass("resizeLogin");
+    $("#navHeader").addClass("resizeNav");
   } else {
-    $("#myHeader").removeClass("sticky");
-    $("#header").removeClass("resizeHeader");
-
-    $("#logo").removeClass("resizeLogo1");
-    $("#logo").addClass("resizeLogo2");
-
-    $("#searchbar").removeClass("resizeBar1");
-    $("#searchbar").addClass("resizeBar2");
-
-    $("#login").removeClass("resizeLogin1");
-    $("#login").addClass("resizeLogin2");
-    
-
-    $("#navHeader").removeClass("navHeader");
+    $("#header").removeClass("transition");
+    $("#header").removeClass("sticky");
+    $("#logo").removeClass("resizeLogo");
+    $("#searchbar").removeClass("resizeSearchBar");
+    $("#login").removeClass("resizeLogin");
+    $("#navHeader").removeClass("resizeNav");
   }
 }
